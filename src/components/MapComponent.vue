7e6f08a36d7c2056e93a54b4c229cef6265ca1f3
Ben Cooley <bcooley@uvm.edu>
2025-09-15 15:34:59 -0400

    revert to default open osm basemap

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -38,12 +38,12 @@ export default {
       // Initialize the map and set a default view
       this.map = L.map('map').setView([39.8283, -98.5795], 4);
 
-      L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.{ext}', {
-        minZoom: 0,
-        maxZoom: 20,
-        attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        ext: 'png'
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      minZoom: 0,
+      maxZoom: 19,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(this.map);
+    }
     },
     fetchDataAndAddMarkers() {
       // Fetch the local CSV file
